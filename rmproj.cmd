dff529d7153d3bdde3107804d839335ea8f6f7fd
Kirisoup <94152497+Kirisoup@users.noreply.github.com>
2025-01-18 14:04:33 +0800

    1

--- rmproj.cmd
+++
@@ -1,8 +0,0 @@
-dotnet new --list | findstr "kiriplugin" >nul
-if %errorlevel% neq 0 (
-    dotnet new install D:\dev\blackhff\template\Kirisoup.PluginTemplate
-)
-cd source
-dotnet new kiriplugin -n %1
-cd ..
-dotnet sln add .\source\%1\%1.csproj
